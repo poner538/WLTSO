81ce044a3def7c10fb374d41722f5abd062b3f3c
poner538 <pontus.erlesand@gmail.com>
2013-12-04 13:19:53 +0100

    commita igen

--- Tower/Tower_test.cc
+++ Tower/Tower_test.cc
@@ -3,9 +3,14 @@
 using namespace std;
 int main()
 {
-    Catapult_tower torn();
-    Shooting_tower torn2();
-    torn.locate_sheep();
+    pos posi;
+    posi.x_pos = 100;
+    posi.y_pos = 25;
+
+
+    Catapult_tower torn(posi);
+    Shooting_tower torn2(posi);
+    torn2.locate_sheep();
    // torn.shoot();
     //cout << torn.dmg << "\n";
     return 0;
---
+++ Tower/main.cc
@@ -0,0 +1,12 @@
+#include "Tower.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+    Catapult_tower torn();
+    Shooting_tower torn2();
+    torn.locate_sheep();
+   // torn.shoot();
+    //cout << torn.dmg << "\n";
+    return 0;
+}

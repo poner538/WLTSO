776ef5a59192141b3f57a4a146339c0e8ff0a0bc
Ema <emabe844@student.liu.se>
2013-12-13 11:55:55 +0100

    En början på game.h

--- Tower/Game.h
+++ Tower/Game.h
@@ -8,3 +8,32 @@
 *
 *
 */
+
+class Game
+{
+public:
+    /*The big 5*/
+    /*Game();
+    ~Game() = default;
+    Game(const Game& other) = delete; //Kopieringskonstruktor
+    Game(const Game&& other) = delete; //Kopieringskonstruktor med move
+    Game& operator=(const Game& other) = delete; //Tilldelningspoerator
+    Game& operator=(const Game&& other) = delete; //Tilldelningspoperator med move*/
+
+
+    /*Medlemsfunktioner*/
+    /*void feed_Sheep(vector<int> current_wave);
+    void game_on(); //Uppdatera grafik, skapa board
+    void game_over();
+    void update_background_graphics();
+    void update_foreground_graphics();*/
+
+private:
+    /*Datamedlemmar*/
+    /*pos course_start_pos; //x-pos, y-pos, course-pos
+    vector<vector<int>> wave;
+    bool start_stop;
+    int current_level;
+    bool can_I_shop;*/
+
+};

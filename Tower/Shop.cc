ee102580a027411ded11e2a2426c7660d091d4ad
Daniel <danbr371@student.liu.se>
2013-12-16 10:28:21 +0100

    Shop kanske

---
+++ Tower/Shop.cc
@@ -0,0 +1,41 @@
+/*
+ * FILNAMN:       Shop.cc
+ * PROGRAMMERARE: Daniel Brattgård 900926-3394, Y3a
+ *                Marie Ekbrant 890401-2740, Y3a
+ * DATUM:         2013-12-
+ *
+ * BESKRIVNING
+ * Handla saker
+ */
+
+//Includes...
+
+
+Shop::Shop();
+~Shop();
+sf::Image ShopImage(bildnamn.jpg)
+sf::Sprite ShopSprite(ShopImage.jpg)
+int price_tower1
+int price_tower2
+
+//Medlemsfunktioner
+void build_tower1() //Öppnas från int main är min tnake i nuläget
+{
+	if(event.mouseButton.button == sf::Mouse::Left)
+	{
+		towerplacement = [event.mouseButton.x, event.mouseButton.y];
+		Board::exist(towerplacement);
+		
+		//Lägg in i rätt ordning
+	}
+    //Kolla om tillräckligt med pengar
+    //Kolla om ledig plats
+}
+ //  Board::exist(pos rightupper_corner);
+ //  Board::exist(pos leftupper_corner);  //Denna ges av musen
+ //  Board::exist(pos rightlower_corner);
+ //  Board::exist(pos leftlower_corner);
+
+
+
+void build_tower2()

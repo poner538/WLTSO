4282bf1eecd8c4e45b7a051c84fd8480ba216905
Ema <emabe844@student.liu.se>
2013-12-18 16:56:13 +0100

    Merge

--- Tower/main.cc
+++ Tower/main.cc
@@ -17,6 +17,75 @@
 #include "Shop.h"
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+
+
+
+
+
+
+struct User
+   {
+      int  user_score = 0;
+      std::string user_name;
+    };
+
+void set_High_Score(string name, int score)
+{
+
+
+    std::vector<User> score_list;
+    std::ifstream Readfile;
+    Readfile.open("High_Score.txt");
+
+    if (Readfile.is_open())
+    {
+        std::string tempname;
+        int tempscore;
+
+        while (Readfile >> tempname >> tempscore)
+        {
+            User temp_user;
+            temp_user.user_score = tempscore;
+             temp_user.user_name = tempname;
+            score_list.push_back(temp_user);
+
+        }
+    }
+    Readfile.close();
+    User new_user;
+    new_user.user_score = score;
+    new_user.user_name = name;
+    for(int i = 0; i < score_list.size(); i++)
+    {
+        if(score_list.at(i).user_score < score)
+        {
+           score_list.insert (score_list.begin() + i, new_user);
+            i = score_list.size();
+        }
+    }
+    if (score_list.empty())
+    {
+        score_list.push_back(new_user);
+    }
+    if(score_list.size() > 10)
+    {
+        score_list.pop_back();
+    }
+    std::ofstream Writefile;
+    Writefile.open("High_Score.txt");
+    for(int i = 0 ; i < score_list.size();i++)
+    {
+        Writefile << score_list.at(i).user_name << " " << score_list.at(i).user_score << endl;
+    }
+    Writefile.close();
+}
+
+
+
+
 
 
 
@@ -200,6 +269,8 @@ int main()
                         myWindow->display();
                         std::cin >> name;
                         won = true;
+                        set_High_Score(name, Controller::controller.get_points());
+
                     }
                     myGame.game_over(name);
                     myGame.change_shopping(false);
@@ -225,3 +296,9 @@ int main()
     }
     return 0;
 }
+
+
+
+
+
+
